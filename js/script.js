1bb20e1bc2665b608df9bd4865005072e1f975d1
Mrigank Pawagi <mrigankpawagi@gmail.com>
2021-08-14 01:23:20 +0530

    cleaning

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 M.AutoInit();
-$("#slide-out").load("components/menu.html");
 $("nav").load("components/nav.html");
 $("footer").load("components/footer.html");
 $("#loader").delay(100).fadeOut(400);
